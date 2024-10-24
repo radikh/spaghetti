519269e554c0d8ff7beefde6ad193eb845601480
user <13639129+radikh@users.noreply.github.com>
2024-10-24 21:45:28 +0300

    Finish rot

---
+++ bf_enc.tcl
@@ -0,0 +1,43 @@
+R!^FDC^3:?^E4=D9
+
+D6E 4@56 ,86ED DE5:?.
+D6E 4@56 ,DA=:E S4@56 QQ.
+D6E 5A _
+D6E :A _
+D6E >6> ,=C6A62E b____ _.
+=2AA6?5 DE24< _
+7@C LD6E :A _N LS:A k ,DEC:?8 =6?8E9 S4@56.N L:?4C :AN L
+    D6E 52E2 ,=:?56I S>6> S5A.
+    D6E 4>5 ,=:?56I S4@56 S:A.
+    DH:E49 S4>5  L
+        QZQ L=D6E >6> S5A ,6IAC WS52E2 Z `XTade.N
+        Q\Q L=D6E >6> S5A ,6IAC WS52E2 \ `XTade.N
+        QmQ L:?4C 5AN
+        QkQ L:?4C 5A \`N
+        Q]Q LAFED \?@?6H=:?6 ,7@C>2E T4 S52E2.N
+        Q,Q L
+            :7 LS52E2 ll _N L
+                D6E :8?@C6 `
+                H9:=6 LS:8?@C6N L
+                    :?4C :A
+                    D6E 4 ,DEC:?8 :?56I S4@56 S:A.
+                    :7 L,DEC:?8 6BF2= S4 L,N .N L
+                        :?4C :8?@C6
+                    N 6=D6:7 L,DEC:?8 6BF2= S4 L.N.N L
+                        :?4C :8?@C6 \`
+                    N
+                N
+            N 6=D6 L
+                =2AA6?5 DE24< ,6IAC S:A \ `.
+            N
+        N
+        Q.Q L
+            :7 LS52E2N L
+                D6E :A ,=:?56I SDE24< 6?5.
+            N
+            D6E DE24< ,=C2?86 SDE24< _ 6?5\`.
+        N
+        5672F=E LN
+    N
+N
+
--- rot13.tcl
+++ rot13.tcl
@@ -20,20 +20,8 @@ set output ""
 
 foreach char $input {
     set a [scan $char %c]
-    if {$a >= 65 && $a <= 90} {
-        set a [expr ($a - 65 + $s) % 26 + 65]
-    } elseif {$a >= 97 && $a <= 122} {
-        set a [expr ($a - 97 + $s) % 26 + 97]
-    } elseif {$a >= 48 && $a <= 57} {
-        set a [expr ($a - 48 + $s) % 10 + 48]
-    } elseif {$a >= 33 && $a <= 47} {
-        set a [expr ($a - 33 + $s) % 15 + 33]
-    } elseif {$a >= 58 && $a <= 64} {
-        set a [expr ($a - 58 + $s) % 7 + 58]
-    } elseif {$a >= 91 && $a <= 96} {
-        set a [expr ($a - 91 + $s) % 6 + 91]
-    } elseif {$a >= 123 && $a <= 126} {
-        set a [expr ($a - 123 + $s) % 4 + 123]
+    if {$a > 33 && $a < 127} {
+        set a [expr {($a + 47 - 33) % 94 + 33}]
     }
 
     set char [format %c $a]
